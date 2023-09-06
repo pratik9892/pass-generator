2f27fd69091fc3e9f9a143d0ffd78981d57d268f
Pratik Sawant <119058935+pratik9892@users.noreply.github.com>
2023-09-06 21:59:46 +0530

    optimzesd for mobile and some tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -36,29 +36,30 @@ function App() {
   return (
     <>
       <div className=" flex items-center justify-center w-full h-screen bg-gradient-to-r from-sky-400 to-cyan-300 ">
-        <div className="sm:w-1/2 sm:h-[500px] backdrop-blur-xl bg-white/30  rounded-lg  flex px-8 justify-center flex-col gap-10 w-full h-[600px] mx-4 border-gray-600 border-2">
+        <div className="sm:w-1/2 sm:h-[550px] backdrop-blur-xl bg-white/30  rounded-lg  flex px-6 justify-center flex-col gap-10 w-full h-[650px] mx-4 sm:border-gray-600 sm:border-2">
           <div className=' py-6'>
             <h1 className="md:text-4xl text-center uppercase text-black font-mono text-6xl">
               Password Generater
             </h1>
           </div>
-          <div className='flex items-center justify-center px-4 py-6 bg-white rounded-xl border-gray-500 border-2'>
+          <div className='flex items-center justify-center px-4 py-6 bg-white rounded-xl border-gray-500 border-2 flex-col sm:flex-row'>
             <input 
             type="text"
             value={password}
             readOnly
-            className='outline-none w-full px-6 py-3 rounded-md font-medium font-mono text-2xl'
+            className='outline-none w-full px-4 py-2 rounded-md font-medium font-mono sm:text-3xl text-2xl'
             placeholder='Password'
             ref={passwordRef}
             />
-            <button className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm sm:px-10 sm:py-4 py-2 px-6'
+            <button className='text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm sm:px-10 sm:py-4 py-2 px-16
+            '
             
             onClick={copyPassToClip}
             >
               COPY
             </button>
           </div>
-          <div className='flex items-center justify-between gap-10 flex-col sm:flex-row bg-white rounded-xl px-4 py-6 border-gray-500 border-2'>
+          <div className='flex items-start sm:items-center justify-between  gap-10 flex-col sm:flex-row bg-white rounded-xl px-4 py-6 border-gray-500 border-2'>
             <div className='flex items-center gap-4'>
               <input 
               type="range"
